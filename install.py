7c0663bcf3def1b9add7e5b10bad8e0267798c4b
tripod31 <tripod31@hotmail.com>
2024-03-21 21:05:40 +0900

    upd

--- install.py
+++ install.py
@@ -3,9 +3,8 @@
 import shutil
 import os
 
-shutil.copy("get_apt_proxy.py",         "/usr/local/bin/")
-with open("/etc/apt/apt.conf.d/01acng","w") as f:
-    f.write('Acquire::http::Proxy-Auto-Detect "/usr/local/bin/get_apt_proxy.py";\n')
+shutil.copy("get_apt_proxy.py", "/usr/local/bin/")
+shutil.copy("01acng",           "/etc/apt/apt.conf.d/")
 
 #disable auto-apt-proxy
 if os.path.exists("/etc/apt/apt.conf.d/auto-apt-proxy.conf"):
