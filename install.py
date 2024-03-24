e0d6e7179d8255c95dd4a68a746fefecd81efb19
tripod31 <tripod31@hotmail.com>
2024-03-24 19:22:44 +0900

    upd

--- install.py
+++ install.py
@@ -3,9 +3,16 @@
 import shutil
 import os
 
-shutil.copy("get_apt_proxy.py", "/usr/local/bin/")
-shutil.copy("01acng",           "/etc/apt/apt.conf.d/")
+def install():
+    shutil.copy("get_apt_proxy.py", "/usr/local/bin/")
+    shutil.copy("01acng",           "/etc/apt/apt.conf.d/")
 
-#disable auto-apt-proxy
-if os.path.exists("/etc/apt/apt.conf.d/auto-apt-proxy.conf"):
-    shutil.move("/etc/apt/apt.conf.d/auto-apt-proxy.conf","/etc/apt/apt.conf.d/auto-apt-proxy.conf.disabled")
+    #disable auto-apt-proxy
+    if os.path.exists("/etc/apt/apt.conf.d/auto-apt-proxy.conf"):
+        shutil.move("/etc/apt/apt.conf.d/auto-apt-proxy.conf","/etc/apt/apt.conf.d/auto-apt-proxy.conf.disabled")
+
+if __name__ == '__main__':
+    try:
+        install()
+    except PermissionError:
+        print("権限がありません")
--- uninstall.py
+++ uninstall.py
@@ -6,12 +6,19 @@ import shutil
 FILES = (
     "/usr/local/bin/get_apt_proxy.py",
     "/etc/apt/apt.conf.d/01acng"
-    )
+)
 
-for file in FILES:
-    if os.path.exists(file):
-        os.remove(file)
+def uninstall():
+    for file in FILES:
+        if os.path.exists(file):
+            os.remove(file)
 
-#enable auto-apt-proxy
-if os.path.exists("/etc/apt/apt.conf.d/auto-apt-proxy.conf.disabled"):
-    shutil.move("/etc/apt/apt.conf.d/auto-apt-proxy.conf.disabled","/etc/apt/apt.conf.d/auto-apt-proxy.conf")
+    #enable auto-apt-proxy
+    if os.path.exists("/etc/apt/apt.conf.d/auto-apt-proxy.conf.disabled"):
+        shutil.move("/etc/apt/apt.conf.d/auto-apt-proxy.conf.disabled","/etc/apt/apt.conf.d/auto-apt-proxy.conf")
+
+if __name__ == '__main__':
+    try:
+        uninstall()
+    except PermissionError:
+        print("権限がありません")
